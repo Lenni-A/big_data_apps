8e26daeb8a47621b9fef1d4bd8bd1d4630d11460
Lenni-A <65432872+Lenni-A@users.noreply.github.com>
2023-10-01 16:42:05 +0200

    Add files via upload

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -17,4 +15,6 @@ def main():
 def echo_input():
     input_text = request.form.get("user_input", "")
     return "You entered: " + input_text
-    
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
